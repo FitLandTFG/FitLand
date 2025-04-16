63f78435584868bb804ffd8f03bf1085cdfa707c
alexp2004 <alejandroprado04@gmail.com>
2025-04-16 17:35:25 +0200

    Campo remember_me insertada en la tabla usuarios

--- app-fitland/database/migrations/2025_04_16_153146_add_remember_token_to_usuarios_table.php
+++ app-fitland/database/migrations/2025_04_16_153146_add_remember_token_to_usuarios_table.php
@@ -12,17 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('usuarios', function (Blueprint $table) {
-            //
+            $table->rememberToken()->nullable();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('usuarios', function (Blueprint $table) {
-            //
+            $table->dropColumn('remember_token');
         });
     }
 };
