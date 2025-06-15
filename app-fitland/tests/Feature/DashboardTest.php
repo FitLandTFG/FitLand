da0727c99f76d45ec466c1e5b478a3c141cb39ad
alexp2004 <alejandroprado04@gmail.com>
2025-06-15 23:04:19 +0200

    Eliminado de tests por defecto

--- app-fitland/tests/Feature/DashboardTest.php
+++ app-fitland/tests/Feature/DashboardTest.php
@@ -2,23 +2,11 @@
 
 namespace Tests\Feature;
 
-use App\Models\User;
+use App\Models\Usuario;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class DashboardTest extends TestCase
 {
-    use RefreshDatabase;
-
-    public function test_guests_are_redirected_to_the_login_page()
-    {
-        $this->get('/inicio')->assertRedirect('/login');
-    }
-
-    public function test_authenticated_users_can_visit_the_dashboard()
-    {
-        $this->actingAs($user = User::factory()->create());
-
-        $this->get('/inicio')->assertOk();
-    }
+    //
 }
