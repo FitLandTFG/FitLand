da50937a4b730d5f7587841f3f1ff167f6c89464
FitLandTFG <fitlandtfg@gmail.com>
2025-06-18 00:41:18 +0200

    Add files via upload

---
+++ app-fitland/public/build/assets/PagoCancelado-CzmwMI8b.js
@@ -0,0 +1,6 @@
+import{j as e}from"./app-CV7uQCN8.js";import{A as t}from"./app-layout-DW-QwsIP.js";import{c as r}from"./createLucideIcon-CbpJX9Ck.js";import"./navbar-B6tlJW2d.js";/**
+ * @license lucide-react v0.475.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"m15 9-6 6",key:"1uzhvr"}],["path",{d:"m9 9 6 6",key:"z0biqf"}]],o=r("CircleX",a);function n(){return e.jsx(t,{children:e.jsxs("div",{className:"min-h-[calc(100vh-80px)] flex flex-col items-center justify-center px-4 text-center bg-white",children:[e.jsx(o,{size:64,className:"text-red-500 mb-4"}),e.jsx("h1",{className:"text-3xl font-extrabold text-red-600 mb-2",children:"Pago cancelado"}),e.jsx("p",{className:"text-gray-700 text-lg mb-6",children:"El proceso de pago fue cancelado o no se completó. Puedes volver al carrito para intentarlo de nuevo."}),e.jsx("a",{href:"/carrito",className:"bg-red-600 hover:bg-red-700 text-white font-semibold px-6 py-2 rounded transition",children:"Volver al carrito"})]})})}export{n as default};
